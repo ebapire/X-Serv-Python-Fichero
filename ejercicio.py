de4034817ef91385604a7c446977d6eb84aeb831
eva garcia domingo <egarcia@zeta40.aulas.gsyc.es>
2016-02-02 12:48:04 +0100

    he terminado el ejercicio

--- ejercicio.py
+++ ejercicio.py
@@ -2,3 +2,12 @@
 # -*- coding: utf-8 -*-
 
 
+fich = open("/etc/passwd", "r")
+lista_usuarios = fich.readlines()
+for linea in lista_usuarios:
+    lista = linea.split(':')
+    print lista[0], lista[-1],
+
+print "la lista tiene ", len(lista_usuarios), "usuarios"
+
+fich.close
